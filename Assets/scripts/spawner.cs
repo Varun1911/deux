cb7cc49d6a2d11017c04f063f7acbf0196aefed2
varun1911 <varunt298@gmail.com>
2020-08-01 11:08:51 +0530

    spawning fixed

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
-    [SerializeField] GameObject[] obstaclePatterns;
+    [SerializeField] GameObject obstacle;
 
     float timeBtwSpawn;
     [SerializeField] float startTimeBtwSpawn;
-    [SerializeField] float decreaseTime;
+    [SerializeField] float decreaseTime = 0.05f;
     [SerializeField] float minTime = 0.65f;
+
+    private void Start()
+    {
+        timeBtwSpawn = startTimeBtwSpawn;
+    }
+
 
     void Update()
     {
         if (timeBtwSpawn <= 0)
         {
-            int rand = Random.Range(0, obstaclePatterns.Length);
-            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
-            Debug.Log(obstaclePatterns[rand]);
+            Vector3 position = new Vector3(Random.Range(-1.16f, 1.16f), transform.position.y, transform.position.z);
+            Instantiate(obstacle, position, Quaternion.identity);
             timeBtwSpawn = startTimeBtwSpawn;
             if(startTimeBtwSpawn > minTime)
             {
