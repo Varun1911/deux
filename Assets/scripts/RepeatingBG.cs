2314f77cecabba8a26afd0399daa35399a59659f
varun1911 <varunt298@gmail.com>
2020-08-01 12:53:41 +0530

    repeating background

---
+++ Assets/scripts/RepeatingBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBG : MonoBehaviour
+{
+    [SerializeField] float bgSpeed = 4f;
+    [SerializeField] float endX = -10f;
+    [SerializeField] float startX = 10f;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * bgSpeed * Time.deltaTime);
+
+        if (transform.position.y <= endX)
+        {
+            Vector2 pos = new Vector2(transform.position.x, startX);
+            transform.position = pos;
+        }
+    }
+}
